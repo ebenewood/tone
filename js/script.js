d865a68b4a74a3f488451434aad623f53a072080
eben <gracejones@Graces-MacBook-Pro.local>
2016-04-21 09:46:38 -0700

    updates some files

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
-playArr = [];
+toneArr = [];
 
 audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
 // create Oscillator node
 oscillator = audioCtx.createOscillator();
 
-function play() {
+oscillator.type = 'triangle';
 
-  oscillator.type = 'triangle';
-  oscillator.frequency.value = parseInt(playArr[0]); // A
+function toneShift() {
 
-  // oscillator.connect(audioCtx.destination);
+  oscillator.frequency.value = parseInt(toneArr[0]);
+
+
+  oscillator.connect(audioCtx.destination);
   oscillator.start();
-  // oscillator.stop(audioCtx.currentTime + 1);
+  oscillator.stop(audioCtx.currentTime + 1);
 
 };
 
